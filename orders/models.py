3f1ec1b12a64c14e61096ab3146ab66dc0a0e68c
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2021-11-30 18:39:21 +0900

    modeling수정본1

--- orders/models.py
+++ orders/models.py
@@ -6,7 +6,7 @@ from users.models    import User
 class Cart(models.Model):
     user    = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    count   = models.IntegerField()
+    count   = models.IntegerField(default=1)
 
     class Meta:
         db_table = 'carts'
@@ -37,3 +37,6 @@ class OrderItems(models.Model):
 
 class OrderItemStatus(models.Model):
     name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'orderitemstatus'
--- products/models.py
+++ products/models.py
@@ -1,11 +1,13 @@
-from django.db import models
+from django.db    import models
+from users.models import User
+
 
 
 class Product(models.Model):
     name              = models.CharField(max_length=100)
     price             = models.DecimalField(max_digits=10, decimal_places=2)
     category          = models.ForeignKey('Category', on_delete=models.CASCADE)
-    rating            = models.IntegerField()
+    rating            = models.IntegerField(null=True)
     image_description = models.URLField()
     create_at         = models.DateTimeField(auto_now_add=True)
     update_at         = models.DateTimeField(auto_now=True)
@@ -25,3 +27,11 @@ class Image(models.Model):
 
     class Meta:
         db_table = 'images'
+
+class Like(models.Model):
+    user    = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    like    = models.NullBooleanField()
+
+    class Meta:
+        db_table = 'likes'
