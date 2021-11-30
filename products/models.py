17b868d384166f1db7874eaf4a35f150f0058e10
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2021-11-30 19:28:32 +0900

    modeling 수정본3

--- products/models.py
+++ products/models.py
@@ -4,13 +4,13 @@ from users.models import User
 
 
 class Product(models.Model):
-    name              = models.CharField(max_length=100)
-    price             = models.DecimalField(max_digits=10, decimal_places=2)
-    category          = models.ForeignKey('Category', on_delete=models.CASCADE)
-    rating            = models.IntegerField(null=True)
-    image_description = models.URLField()
-    create_at         = models.DateTimeField(auto_now_add=True)
-    update_at         = models.DateTimeField(auto_now=True)
+    name               = models.CharField(max_length=100)
+    price              = models.DecimalField(max_digits=10, decimal_places=2)
+    category           = models.ForeignKey('Category', on_delete=models.CASCADE)
+    rating             = models.IntegerField(null=True)
+    image_description  = models.URLField()
+    created_at         = models.DateTimeField(auto_now_add=True)
+    updated_at         = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'products'
@@ -31,7 +31,6 @@ class Image(models.Model):
 class Like(models.Model):
     user    = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
-    like    = models.NullBooleanField()
 
     class Meta:
         db_table = 'likes'
--- users/models.py
+++ users/models.py
@@ -3,13 +3,13 @@ from products.models import Product
 
 
 class User(models.Model):
-    name      = models.CharField(max_length=100)
-    email     = models.CharField(max_length=100)
-    password  = models.CharField(max_length=200)
-    contact   = models.CharField(max_length=100)
-    address   = models.CharField(max_length=100)
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    name       = models.CharField(max_length=100)
+    email      = models.CharField(max_length=100, unique=True)
+    password   = models.CharField(max_length=200)
+    contact    = models.CharField(max_length=100)
+    address    = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Meta:
     db_table = 'users'
