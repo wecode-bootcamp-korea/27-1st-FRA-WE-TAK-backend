317f1319013be9b89e72917dfda168df3a897873
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2021-11-30 17:02:35 +0900

    modeling

--- models/models.py
+++ models/models.py
@@ -1,15 +1,14 @@
 from django.db import models
-from django.db.models.aggregates import Max
 from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
 class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    password= models.CharField(max_length=200)
-    contact = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
+    name      = models.CharField(max_length=100)
+    email     = models.CharField(max_length=100)
+    password  = models.CharField(max_length=200)
+    contact   = models.CharField(max_length=100)
+    address   = models.CharField(max_length=100)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
@@ -18,16 +17,16 @@ class User(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey('Products_Category',on_delete=CASCADE)
-    rating = models.IntegerField()
+    name              = models.CharField(max_length=100)
+    price             = models.DecimalField(max_digits=10, decimal_places=2)
+    category          = models.ForeignKey('Products_Category',on_delete=CASCADE)
+    rating            = models.IntegerField()
     image_description = models.URLField()
-    create_at= models.DateTimeField(auto_now_add=True)
-    update_at= models.DateTimeField(auto_now=True)
+    create_at         = models.DateTimeField(auto_now_add=True)
+    update_at         = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table='products'
+        db_table = 'products'
 
 class Products_Category(models.Model):
     name = models.CharField(max_length=100)
@@ -37,13 +36,13 @@ class Products_Category(models.Model):
 
 class Image(models.Model):
     product = models.ForeignKey('Product',on_delete=CASCADE)
-    image = models.URLField()
+    image   = models.URLField()
 
     class Meta:
         db_table='images'
 
 class Review(models.Model):
-    user = models.ForeignKey('User', on_delete=CASCADE)
+    user    = models.ForeignKey('User', on_delete=CASCADE)
     product = models.ForeignKey('Product', on_delete=CASCADE)
     content = models.TextField()
 
@@ -51,16 +50,16 @@ class Review(models.Model):
         db_table='reviews'
 
 class Cart(models.Model):
-    user = models.ForeignKey('User',on_delete=CASCADE)
+    user    = models.ForeignKey('User',on_delete=CASCADE)
     product = models.ForeignKey('Product', on_delete=CASCADE)
-    count = models.IntegerField()
+    count   = models.IntegerField()
 
     class Meta:
         db_table='carts'
 
 class Order(models.Model):
     order_number = models.IntegerField()
-    user = models.ForeignKey('User',on_delete=CASCADE)
+    user         = models.ForeignKey('User',on_delete=CASCADE)
     order_status = models.ForeignKey('',on_delete=CASCADE)
 
     class Meta:
@@ -73,10 +72,10 @@ class Order_status(models.Model):
         db_table='order_status'
 
 class Order_items(models.Model):
-    order = models.ForeignKey('Order', on_delete=CASCADE)
-    count = models.IntegerField()
-    tracking_number = models.CharField(max_length=200)
-    product = models.ForeignKey('Product',on_delete=CASCADE)
+    order             = models.ForeignKey('Order', on_delete=CASCADE)
+    count             = models.IntegerField()
+    tracking_number   = models.CharField(max_length=200)
+    product           = models.ForeignKey('Product',on_delete=CASCADE)
     order_item_status = models.ForeignKey('Order_item_status',on_delete=CASCADE)
 
     class Meta:
