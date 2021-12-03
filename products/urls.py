c96ad7bd255b707b0d8b2a088049aed60fb900f9
Jaeyong Park <jaejayjaejaypark@gmail.com>
2021-12-03 23:45:32 +0900

    wait

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
-from .views      import RandomCategoryView
+from .views      import ListPageView
 
 urlpatterns = [
+    path('/list_page', ListPageView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http.response  import JsonResponse
+from django.views          import View
+from django.http.response  import JsonResponse
+from .models               import Product 
 
-# Create your views here.
+
+class ListPageView(View):
+    def get(self, request, subcategory_id):
+        items = Product.objects.filter(sub_category__id = subcategory_id).all()
+        results = []
+        for i in items:
+                results.append(
+                    {
+                        'id'            : i.id,
+                        'kr_name'       : i.kr_name,
+                        'thumbnail_url' : i.thumbnail_url,
+                    }
+                )
+        return JsonResponse({"result":results}, status=200) 
