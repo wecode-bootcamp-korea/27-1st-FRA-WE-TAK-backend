c75ee3331ff2889da385c578fde997257004dec2
유현이 <yu@yuhyeon-iui-MacBookPro.local>
2021-12-05 15:04:37 +0900

    [MODIFIED]:코드수정

--- users/views.py
+++ users/views.py
@@ -14,10 +14,10 @@ class LoginView(View):
             user_password  = data['password']
             user           = User.objects.get(email=user_email)
 
-            if bcrypt.checkpw(user_password.encode('utf-8'), user.password.encode('utf-8')):
+            if not bcrypt.checkpw(user_password.encode('utf-8'), user.password.encode('utf-8')):
                 return JsonResponse({"message": "invalid_password"}, status=401)
 
-            token = jwt.encode ({'id':user.id}, SECRET_KEY, ALGORITHM=ALGORITHM)
+            token = jwt.encode ({'id':user.id}, SECRET_KEY, algorithm=ALGORITHM)
 
             return JsonResponse({
                 'message' : 'success', 
