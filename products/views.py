619672e5f896a2a406077585b2554a1a1aa68e99
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2021-12-06 20:50:55 +0900

    검색기능구현

--- products/views.py
+++ products/views.py
@@ -28,16 +28,16 @@ class CategoryView(View):
 class SearchView(View):
     def get(self, request):
         data    = json.loads(request.body)
-        products = Product.objects.filter(kr_name=data['search'])
+        products = Product.objects.filter(kr_name__icontains=data['search'])
         results = []
 
         for product in products:
-            results=[{
+            results.append([{
                     "product_id"      : product.id,
                     "product_kr_name" : product.kr_name,
                     "product_en_name" : product.en_name,
                     "product_price"   : product.price,
                     "product_image"   : [image.url for image in product.image_set.all()]
-            }]
+            }])
         return JsonResponse({'result' : results}, status=200)
 
