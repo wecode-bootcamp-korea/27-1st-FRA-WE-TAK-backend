f1792054b468f6eaa0d197ef739270c02f920f07
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2021-12-06 17:27:13 +0900

    임시

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views      import RandomCategoryView
+from .views      import SearchView
 
 urlpatterns = [
+    path('/search', SearchView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http.response import JsonResponse
+from django.views         import View
+
+from .models              import Product
+
+class SearchView(View):
+    def get(self, request):
+        data    = json.loads(request.body)
+        products = Product.objects.filter(kr_name = data['search'])
+        for product in products:
+            result=[{
+                    "product_id"      : product.product.id,
+                    "product_kr_name" : product.product.kr_name,
+                    "product_en_name" : product.product.en_name,
+                    "product_price"   : product.product.price,
+                    "product_image"   : [image.url for image in product.image_set.all()]
+            }]
+        return JsonResponse({'result' : result}, status=200)
