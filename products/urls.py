15dfce95846be1ba66a4e97f2b98dcad45a5d492
Jaeyong Park <jaejayjaejaypark@gmail.com>
2021-12-06 20:30:27 +0900

    테스트

--- products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls     import path, include
-from products.views  import ProductDetailView, SubCategoryView
-
-urlpatterns = [
-    path('/<int:sub_category_id>', SubCategoryView.as_view()),
-    path('/subcategory/<int:product_id>', ProductDetailView.as_view())
-]
