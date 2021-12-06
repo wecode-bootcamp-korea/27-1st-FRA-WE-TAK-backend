7ce3f015a201bf4a95fa3f68ffc9fc1a3090653b
Jaeyong Park <jaejayjaejaypark@gmail.com>
2021-12-06 14:45:02 +0900

    ListPageView 수정

--- products/views.py
+++ products/views.py
@@ -7,23 +7,23 @@ from products.models       import Product
 class ListPageView(View):
     def get(self, request, sub_category_id):
         products = Product.objects.filter(sub_category_id = sub_category_id)
-        print(sub_category_id)
         results =[]
         for product in products:
-                results.append(
-                    {
-                        'product_id'        : product.id,
-                        'kr_name'           : product.kr_name,
-                        'en_name'           : product.en_name,
-                        'price'             : product.price,
-                        'sub_category_id'   : product.sub_category.id,
-                        'sub_category_name' : product.sub_category.kr_name,
+            results.append(
+                {
+                        'product_id'         : product.id,
+                        'kr_name'            : product.kr_name,
+                        'en_name'            : product.en_name,
+                        'price'              : product.price,
+                        'sub_category_id'    : product.sub_category.id,
+                        'sub_category_name'  : product.sub_category.kr_name,
+                        'main_category_name' : product.sub_category.main_category.name,
+                        'main_category_id'   : product.sub_category.main_category.id,
                         'images' : [{
-                            "image_id"      : image.id,
-                            "product_image" : image.url,
+                            "image_id"       : image.id,
+                            "product_image"  : image.url,
                             } for image in product.image_set.all()],
                         'rating'        : product.rating
                     }
-                )
-        
-        return JsonResponse({"result":results}, status=200) 
+            )
+        return JsonResponse({"result":results}, status=200)
