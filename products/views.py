12ee17d75557e01f0f22e63dc5931b812053e9b3
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2021-12-06 15:12:37 +0900

    mainpage 일부 수정

--- products/views.py
+++ products/views.py
@@ -8,21 +8,21 @@ from .models               import MainCategory
 
 class CategoryView(View):
     def get(self, request):
-        maincategories        = MainCategory.objects.prefetch_related('subcategory_set')
+        main_categories       = MainCategory.objects.prefetch_related('subcategory_set')
         results = []
-        for maincategory in maincategories:
-            subcategories     = maincategory.subcategory_set.all()
+        for main_category in main_categories:
+            subcategories     = main_category.subcategory_set.all()
             sub_category_list = []
-            for subcategory in subcategories:
-                sub_category_list.append(
-                    {
-                        'id'            : subcategory.id,
-                        'kr_name'       : subcategory.kr_name,
-                        'thumbnail_url' : subcategory.thumbnail_url,
-                    }
-                )
+            # for subcategory in subcategories:
+                # sub_category_list.append(
+                #     {
+                #         'id'            : subcategory.id,
+                #         'kr_name'       : subcategory.kr_name,
+                #         'thumbnail_url' : subcategory.thumbnail_url,
+                #     }
+                # )
             results.append({
-                'name'                  : maincategory.name,
-                'sub_category_list'     : sub_category_list
+                'name'                  : main_category.name,
+                'sub_category_list'     : [sub_category_list for sub_category_list in subcategories]
             })
         return JsonResponse({"result":results}, status=200) 
