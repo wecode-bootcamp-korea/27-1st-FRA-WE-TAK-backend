7fb7fcce6aa28bcbfeeb7e10a51d41ccbb7dc9fc
yajkrap <93360693+yajkrap@users.noreply.github.com>
2021-12-07 11:51:41 +0900

    Update views.py

--- products/views.py
+++ products/views.py
@@ -1,24 +1,7 @@
 from django.http.response  import JsonResponse
 from django.views          import View
 from django.http.response  import JsonResponse
-from products.models       import Product, MainCategory
-
-class CategoryView(View):
-    def get(self, request):
-        main_categories       = MainCategory.objects.prefetch_related('subcategory_set')
-        results = []
-
-        for main_category in main_categories:
-            sub_categories     = main_category.subcategory_set.all()
-            results.append({
-                'name'                  : main_category.name,
-                'sub_category_list'     : [{
-                    'id'                : sub_category.id, 
-                    'kr_name'           : sub_category.kr_name, 
-                    'thumbnail_url'     : sub_category.thumbnail_url
-                    } for sub_category in sub_categories]
-            })
-        return JsonResponse({"result":results}, status=200) 
+from products.models       import Product
 
 class ProductListView(View):
     def get(self, request):
