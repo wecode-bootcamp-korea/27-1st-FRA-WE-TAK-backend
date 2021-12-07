0354196d02ae12d3aed14900cb887d8973a5b784
Jaeyong Park <jaejayjaejaypark@gmail.com>
2021-12-07 21:24:56 +0900

    ProductView Modified

--- products/views.py
+++ products/views.py
@@ -44,19 +44,22 @@ class ProductListView(View):
         return JsonResponse({"result":results}, status=200)
 
 class ProductView(View):
+    # 8000:products/1
     def get(self, request, product_id):
-        products = Product.objects.select_related("sub_category").get(id = product_id)
-        results =[{
-            'product_id'           : product.id,
-            'kr_name'              : product.kr_name,
-            'en_name'              : product.en_name,
-            'price'                : product.price,
-            'sub_category_id'      : product.sub_category.id,
-            'sub_category_kr_name' : product.sub_category.kr_name,
-            'sub_category_en_name' : product.sub_category.en_name,
-            'rating'               : product.rating,
-            'description_txt'      : product.title,
-            'description_img'      : product.description
-        } for product in products]
-        return JsonResponse({"result":results}, status=200)
-        
+        try:
+            product = Product.objects.select_related("sub_category").get(id = product_id)
+
+            result = {
+                'product_id'        : product.id,
+                'kr_name'           : product.kr_name,
+                'en_name'           : product.en_name,
+                'price'             : product.price,
+                'sub_category_id'   : product.sub_category.id,
+                'sub_category_name' : product.sub_category.kr_name,
+                'rating'            : product.rating,
+                'description_img'   : product.description
+            }
+        except Product.DoesNotExist:
+            return JsonResponse({"message": 'Product_Not_Exists'}, status=404)
+
+        return JsonResponse({"result":result}, status=200) 
