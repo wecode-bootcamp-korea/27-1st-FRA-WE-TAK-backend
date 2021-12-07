7c6c867ae8d4ea78962654dfc40f0088316f7db2
lordmyshepherd <lordmyshepherd29@gmail.com>
2021-12-07 16:33:54 +0900

    WIP

--- products/views.py
+++ products/views.py
@@ -1,6 +1,5 @@
 from django.http.response  import JsonResponse
 from django.views          import View
-from django.http.response  import JsonResponse
 from products.models       import Product
 
 class ProductListView(View):
@@ -23,3 +22,25 @@ class ProductListView(View):
             }for product in products]
             
         return JsonResponse({"result":results}, status=200)
+
+
+class ProductView(View):
+    # 8000:products/1
+    def get(self, request, product_id):
+        try:
+            product = Product.objects.select_related("sub_category").get(id = product_id)
+
+            result = {
+                'product_id'        : product.id,
+                'kr_name'           : product.kr_name,
+                'en_name'           : product.en_name,
+                'price'             : product.price,
+                'sub_category_id'   : product.sub_category.id,
+                'sub_category_name' : product.sub_category.kr_name,
+                'rating'            : product.rating,
+                'description_img'   : product.description
+            }
+        except Product.DoesNotExist:
+            return ..
+
+        return JsonResponse({"result":result}, status=200) 
