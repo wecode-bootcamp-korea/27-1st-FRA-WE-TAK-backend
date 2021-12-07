1d52f9950d244e05c4822a922f9ce66224660237
lordmyshepherd <lordmyshepherd29@gmail.com>
2021-12-07 16:21:09 +0900

    WIP

--- products/views.py
+++ products/views.py
@@ -1,25 +1,31 @@
 from django.http.response  import JsonResponse
 from django.views          import View
-from django.http.response  import JsonResponse
+
 from products.models       import Product
 
 class ProductListView(View):
+    # :8000/products?offset=0&limit=100
+    # :8000/products?offset=100&limit=100
+    # :8000/products?offset=200&limit=100
     def get(self, request):
-        products = Product.objects.all()
+        offset   = request.GET.get("offset", 1)
+        limit    = request.GET.get("limit", 100)
+        products = Product.objects.all()[offset:offset+limit]
+
         results =[{
-                'product_id'         : product.id,
-                'kr_name'            : product.kr_name,
-                'en_name'            : product.en_name,
-                'price'              : product.price,
-                'sub_category_id'    : product.sub_category.id,
-                'sub_category_name'  : product.sub_category.kr_name,
-                'main_category_name' : product.sub_category.main_category.name,
-                'main_category_id'   : product.sub_category.main_category.id,
-                'images' : [{
-                    "image_id"       : image.id,
-                    "product_image"  : image.url,
-                    } for image in product.image_set.all()],
-                'rating'        : product.rating
-            }for product in products]
-            
+            'id'                 : product.id,
+            'kr_name'            : product.kr_name,
+            'en_name'            : product.en_name,
+            'price'              : product.price,
+            'sub_category_id'    : product.sub_category.id,
+            'sub_category_name'  : product.sub_category.kr_name,
+            'main_category_name' : product.sub_category.main_category.name,
+            'main_category_id'   : product.sub_category.main_category.id,
+            'rating'             : product.rating
+            'images'             : [{
+                "id"  :  image.id,
+                "url" : image.url,
+            } for image in product.image_set.all()],
+        } for product in products]
+        
         return JsonResponse({"result":results}, status=200)
