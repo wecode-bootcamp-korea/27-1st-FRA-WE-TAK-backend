9b20f8b525b6208cf07a4848e74e6c764d7f9bf3
Jaeyong Park <jaejayjaejaypark@gmail.com>
2021-12-07 11:38:30 +0900

    ProductListView Modified

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,7 @@
 from django.urls import path
-<<<<<<< HEAD
-
-urlpatterns = [
-   # path('/signup', SignUpView.as_view()),
-   # path('/login', LoginView.as_view())
-]
-
-=======
 from .views      import SignUpView, LoginView
 
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
     path('/login', LoginView.as_view())
 ]
->>>>>>> main
