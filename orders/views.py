cafa13e0656b4a588ab4f216a32c086956309cb7
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2021-12-09 10:17:12 +0900

    기능구현중

--- orders/views.py
+++ orders/views.py
@@ -10,13 +10,13 @@ from FRA_WE_BACK.core.utils import log_in_decorator
 class CartView(View):
     @log_in_decorator
     def get(self, request):
-            carts  = Cart.objects.filter(user=1).select_related("product").prefetch_related("product__image_set")
+            carts  = Cart.objects.filter(user=request.user).select_related("product").prefetch_related("product__image_set")
     
             results = [{
                 "cart_id"         : cart.id,
-                # "user_id"         : request.user.id,
-                # "email"           : request.user.email,
-                # "name"            : request.user.name,
+                "user_id"         : request.user.id,
+                "email"           : request.user.email,
+                "name"            : request.user.name,
                 "product_id"      : cart.product.id,
                 "product_kr_name" : cart.product.kr_name,
                 "product_en_name" : cart.product.en_name,
@@ -33,7 +33,7 @@ class CartView(View):
             data               = json.loads(request.body)
 
             Cart.objects.create(
-                # user           = request.user,
+                user           = request.user,
                 product_id     = data['product_id'],
                 count          = data['count']
             )
@@ -46,9 +46,7 @@ class CartView(View):
     def patch(self, request, cart_id):
         try:
             data = json.loads(request.body)
-            print(cart_id)
-            cart = Cart.objects.get(id=cart_id)
-            # cart = Cart.objects.get(id=cart_id, user_id= request.user.id)
+            cart = Cart.objects.get(id=cart_id, user_id= request.user.id)
             cart.count = data["count"]
             cart.save()
            
@@ -64,30 +62,41 @@ class CartView(View):
         return JsonResponse({'message':'NO CONTENT'}, status=204)
 
 
-# class OrderView(View):
-#     @log_in_decoratorr
-#     def post(self, request):
-#         data       = json.loads(request.body)
-#         user       = request.user
-#         order_item = OrderItem.objects.select_related("product","order")
-
-#         Order.objects.create(
-#             {
-#                 "order_number"    : uuid.uuid4,
-#                 "order_status_id" : 1,
-#                 "user_id"         : user.id   
-#             }
-#         )
-
-#         OrderItem.objects.create(
-#             {
-#                 "order"              : a,            
-#                 "count"              : a,
-#                 "tracking_number"    : a,
-#                 "product"            : OrderItem.product,
-#                 "order_item_status"  : 1,
-#                 "address"            : data["address"]
-#             }
-#         )
+class OrderStatus(Enum):    
+    WAIT_DEPOSIT       = 1   
+    COMPLETION_DEPOSIT = 2    
+    READY_RELEASE      = 3  
+    SHIPMENT_COMPLETE  = 4
+    DELIVERED          = 5
+    DELAYED_DELIVERY   = 6
+    DELIVERY_COMPLETED = 7
+    ORDER_COMPLETE     = 8
+    ORDER_CANCELLATION = 9    
+class OrderView(View):
+    @log_in_decorator
+    def post(self, request):
+        try:
+            data       = json.loads(request.body)
+            user       = request.user
+            order = Order.objects.create(
+                {
+                    "order_number"    : uuid.uuid4(),
+                    "order_status_id" : 1,
+                    "user_id"         : user.id   
+                }
+            )
+            bulk_list = [OrderItem(
+                {   
+                    "order_item_status"  : 1,            
+                    "count"              : data["count"],
+                    "tracking_number"    : uuid.uuid4(),
+                    "address"            : data["address"],
+                    "product"            : [{"product_id" :OrderItem.product.id} for i in OrderItem.product],
+                    "order"              : order.id,
+                }for carts in Cart)]
+            
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+    # def get(self, request):
 
 
